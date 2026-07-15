0b045f53e92a2ff5a6302bad00f838b0132b9485
Youssef <youssefups1@gmail.com>
2026-07-15 23:53:22 +0300

    constellation_sink: implement complex-only IQ scatter sink

--- include/gnuradio/cyberether/cyber_constellation_sink.h
+++ include/gnuradio/cyberether/cyber_constellation_sink.h
@@ -1,6 +1,6 @@
 /* -*- c++ -*- */
 /*
- * Copyright 2026 Youssef.
+ * Copyright 2026 Youssef Mahmoud.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
@@ -9,31 +9,27 @@
 #define INCLUDED_CYBERETHER_CYBER_CONSTELLATION_SINK_H
 
 #include <gnuradio/cyberether/api.h>
+#include <gnuradio/gr_complex.h>
 #include <gnuradio/sync_block.h>
 
 namespace gr {
   namespace cyberether {
 
     /*!
-     * \brief <+description of block+>
-     * \ingroup cyberether
-     *
+     * \brief IQ constellation (scatter) sink: real -> X, imag -> Y.
+     * Complex input only. The window is opened by cyberether.present().
      */
     class CYBERETHER_API cyber_constellation_sink : virtual public gr::sync_block
     {
      public:
       typedef std::shared_ptr<cyber_constellation_sink> sptr;
 
-      /*!
-       * \brief Return a shared_ptr to a new instance of cyberether::cyber_constellation_sink.
-       *
-       * To avoid accidental use of raw pointers, cyberether::cyber_constellation_sink's
-       * constructor is in a private implementation
-       * class. cyberether::cyber_constellation_sink::make is the public interface for
-       * creating new instances.
-       */
-      static sptr make();
+      static sptr make(size_t buffer_size = 2048,
+                       const std::string& name = "constellation",
+                       const std::string& gui_hint = "");
     };
+
+    typedef cyber_constellation_sink cyber_constellation_sink_c;
 
   } // namespace cyberether
 } // namespace gr
--- lib/cyber_constellation_sink_impl.h
+++ lib/cyber_constellation_sink_impl.h
@@ -1,6 +1,6 @@
 /* -*- c++ -*- */
 /*
- * Copyright 2026 Youssef.
+ * Copyright 2026 Youssef Mahmoud.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
@@ -9,6 +9,8 @@
 #define INCLUDED_CYBERETHER_CYBER_CONSTELLATION_SINK_IMPL_H
 
 #include <gnuradio/cyberether/cyber_constellation_sink.h>
+#include <jetstream/superluminal.hh>      //cyberether's superluminal plotting
+
 
 namespace gr {
   namespace cyberether {
@@ -16,18 +18,26 @@ namespace gr {
     class cyber_constellation_sink_impl : public cyber_constellation_sink
     {
      private:
-      // Nothing to declare in this block.
+         const uint64_t    d_buffer_size;
+         const std::string d_name;
+         const std::string d_gui_hint;            // "row, col[, spans]"; empty = auto
+         uint64_t          d_display_write_ptr;   // rolling write head into d_tensor
+         Jetstream::Tensor d_tensor;              // display buffer, CF32, shape {1, N};
+                                                  // written by work(), read in place by Superluminal
 
      public:
-      cyber_constellation_sink_impl();
-      ~cyber_constellation_sink_impl();
+      cyber_constellation_sink_impl(size_t buffer_size, const std::string& name,
+                                    const std::string& gui_hint);
+      ~cyber_constellation_sink_impl() override;
 
-      // Where all the action really happens
+      bool start() override;             // GR lifecycle: register plot with the context
+      bool stop() override;              // GR lifecycle: unregister plot
+
       int work(
               int noutput_items,
               gr_vector_const_void_star &input_items,
               gr_vector_void_star &output_items
-      );
+      ) override;
     };
 
   } // namespace cyberether
